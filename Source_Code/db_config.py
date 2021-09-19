d830578d8e838545beb6ec22065468daf39a6d85
Brandon Mack <bk.mack@yahoo.com>
2021-09-19 17:04:43 -0400

    Added missing comment to db_config file

--- Source_Code/db_config.py
+++ Source_Code/db_config.py
@@ -17,6 +17,7 @@ encPW = fernet.encrypt(raw_pw.encode())
 
 #--------------------------------------------------------
 
+# database connection variable mappings
 user = os.environ.get("PYTHON_USER", "bmack4")
 dsn = os.environ.get("PYTHON_CONNECT_SSTRING", "artemis.vsnet.gmu.edu:1521/vse18c.vsnet.gmu.edu")
 pw = os.environ.get("PYTHON_PASSWORD", fernet.decrypt(encPW).decode())
